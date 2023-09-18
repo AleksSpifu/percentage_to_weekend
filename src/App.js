7f065bf899382a5779cbd9b7ca3ae6631566c94d
AleksSpifu <66044119+AleksSpifu@users.noreply.github.com>
2023-09-18 10:16:43 +0200

    update to use json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import WorldTimeComponent from './WorldTimeComponent';
-
+/*
 const JUST_LIKE_THAT_WEEKEND_GONE = "https://images.prismic.io/friday-marketing/5469e8fd-6de1-482f-88db-ec69d0c139f8_funny-weekend-memes-48-720x847.jpg"
 const I_SMELL_THE_WEEKEND = "https://www.digitalmomblog.com/wp-content/uploads/2021/10/smell-weekend-meme.jpg"
 const STAY_STRONG_WEEKEND_SOON = "https://facultyloungers.com/cdn/shop/articles/Weekend-soon-baby-teacher-meme.jpg"
@@ -11,97 +11,52 @@ const REALIZING_ITS_TUESDAY = "https://media.tenor.com/YQBpwdjkwZsAAAAd/pam-tues
 const MICHAEL_SCOTT_DANCE = "https://media1.giphy.com/media/oUZDGQg4XNotM6SvH0/giphy.gif?cid=ecf05e47knvg0c4yeoo2reqhe6uxlv84ng5e6q75a5bybgin&ep=v1_gifs_search&rid=giphy.gif&ct=g"
 const WOAH_HALFWAY_THERE = "https://i.pinimg.com/736x/cc/49/2c/cc492c3c4ce663df412c159a49db5e17.jpg"
 const DRUMROLL = "https://i.giphy.com/media/CLCT8BXR1T2oyBYilM/giphy.webp"
+*/
 const Weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-const TimeZoneDisplayer = () => {
-  useEffect(() => {
-    const today = new Date('2023-09-06T00:00:00')
-    const hrs = today.getHours()
-    console.log(hrs)
-    const offsets = [-7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
-  }, [])
-  return (
-    <div></div>
-  )
-}
 
 
 function App() {
   const [today, setToday] = useState(new Date())
   const [percentage, setPercentage] = useState(0)
+  const lastMinute = useRef(-10)
 
   const [isWeekend, setIsWeekend] = useState(false)
 
   const [imgurl, setImgurl] = useState(null)
 
-  const selectImage = () => {
-    // Is it monday before 8 in the morning?
-    if (today.getDay() === 1 && today.getHours() < 8) {
-      setImgurl(FINALLY_WEEKEND)
-    }
-
-    // Is it monday morning?
-    else if (today.getDay() === 1 && today.getHours() < 12) {
-      setImgurl(JUST_LIKE_THAT_WEEKEND_GONE)
-    }
-
-    // Is it tuesday?
-    else if (today.getDay() === 2 && (today.getMinutes() % 10 === 0)) {
-      setImgurl(REALIZING_ITS_TUESDAY)
-    }
-
-    // Is it wednesday after noon?
-    else if (today.getDay() === 3 && today.getHours() >= 12) {
-      setImgurl(WOAH_HALFWAY_THERE)
-    }
-
-    // Is it Thursday?
-    else if (today.getDay() === 4) {
-      setImgurl(STAY_STRONG_WEEKEND_SOON)
-    }
-
-    // Is it friday before noon?
-    else if (today.getDay() === 5 && today.getHours() < 12) {
-      setImgurl(STAY_STRONG_WEEKEND_SOON)
-    }
-
-    // Is it friday just before 16?
-    else if (today.getDay() === 5 && today.getHours() === 15 && today.getMinutes() > 58) {
-      setImgurl(DRUMROLL)
-    }
-
-    // Is it friday after noon?
-    else if (today.getDay() === 5 && today.getHours() >= 12 && today.getHours() < 16) {
-      setImgurl(I_SMELL_THE_WEEKEND)
-    }
-
-    // Or is it friday after 16?
-    else if (today.getDay() === 5 && today.getHours() >= 16 && today.getMinutes() < 5) {
-      setImgurl(MICHAEL_SCOTT_DANCE)
-    }
-
-    // Or is it friday after 16?
-    else if (today.getDay() === 5 && today.getHours() >= 16) {
-      setImgurl(FINALLY_WEEKEND)
-    }
-
-    // Is it the weekend?
-    else if (today.getDay() === 6 || today.getDay() === 0) {
-      setImgurl(FINALLY_WEEKEND)
-    }
-
-
-
-    // If none, set img to null and dont show anything
-    else {
+  const updateImage = async () => {
+    const res = await fetch("https://raw.githubusercontent.com/AleksSpifu/percentage_to_weekend/main/public/images.json")
+    const json = await res.json()
+    const todaysImages = json[Weekdays[today.getDay()]]
+    const todaysHoursInFloat = today.getHours() + (today.getMinutes() / 60)
+    console.log(todaysHoursInFloat)
+    const currentImage = todaysImages.find((e) => {
+      const startTimeFloat = e.StartHour + (e.StartMinute / 60)
+      const endTimeFloat = e.EndHour + (e.EndMinute / 60)
+      return startTimeFloat <= todaysHoursInFloat && endTimeFloat > todaysHoursInFloat
+    })
+    console.log(currentImage)
+    if (!currentImage) {
       setImgurl(null)
+      return
     }
+    if (currentImage.IsGif) {
+      if (today.getMinutes() % 10 === 0) {
+        setImgurl(currentImage.URL)
+      } else {
+        setImgurl(null)
+      }
+    } else {
+      setImgurl(currentImage.URL)
+    }
+
   }
 
   const update = () => {
     setToday(new Date())
-    selectImage()
+    //selectImage()
     if (today.getDay() === 6 || today.getDay() === 0) {
       setIsWeekend(true)
       return
@@ -144,6 +99,10 @@ function App() {
 
 
   useEffect(() => {
+    if (lastMinute.current !== today.getMinutes()) {
+      lastMinute.current = today.getMinutes()
+      updateImage()
+    }
     const timer = setTimeout(update, 1000)
     return (() => {
       clearTimeout(timer)
